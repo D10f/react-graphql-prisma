cdb5d3ffcc3e71de0da08bcb413b85eb61d9925f
herokunt <galactodensimeter_bismuthyl@simplelogin.fr>
2021-09-02 19:05:45 +0200

    add sanitization before saving and front end small refactor

---
+++ client/src/pages/Feeds/FeedPage.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from 'react';
+import { useLocation } from "react-router-dom";
+import { useQuery, useLazyQuery, useReactiveVar } from '@apollo/client';
+import mapLocationToProps from '@utils/mapLocationToProps';
+import InfiniteScroll from '@components/InfiniteScroll';
+import Grid from './Grid';
+
+import { PER_PAGE } from '@constants';
+
+/**
+ * Queries the server and retrieves posts data to be displayed as a feed.
+ * The graphql query string and cache received as props are based on the current route.
+ * @param {function} cache apollo-client's reactive variable instance
+ * @param {string}   query graphql query string built using apollo-client's gql
+ */
+const Feed = () => {
+
+  const location = useLocation();
+  const { cache, query } = mapLocationToProps(location);
+
+  const postsInCache = useReactiveVar(cache);
+
+  // The name of the query value, varies depending on the query string used to fetch data
+  const queryName = query.definitions[0].name.value;
+
+  // const { data, loading, error, fetchMore } = useQuery(query, {
+  const [ startQuery, { data, loading, error }] = useLazyQuery(query, {
+    onCompleted: (responseData) => {
+
+      const newUniqueValues = responseData[queryName].filter(post => {
+        return !postsInCache.some(cachedPost => cachedPost.id === post.id);
+      });
+
+      cache([
+        ...postsInCache,
+        ...newUniqueValues
+      ]);
+    }
+  });
+
+  // Make initial server request
+  useEffect(() => {
+    startQuery({ variables: { limit: PER_PAGE, skip: postsInCache.length }, fetchPolicy: 'network-only' });
+  }, []);
+
+  return (
+    <InfiniteScroll
+      nextItems={data && data[queryName]}
+      fetchNext={() => {
+        startQuery({ variables: { limit: PER_PAGE, skip: postsInCache.length }});
+      }}
+      loading={loading}
+      error={error}
+    >
+      <Grid items={postsInCache} />
+    </InfiniteScroll>
+  );
+};
+
+export default Feed;
