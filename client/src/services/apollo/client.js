fd07051cfb3e9758fffe004b0dc1e03a79e678cc
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2022-08-02 18:16:33 +0200

    Made apollo-client uri flexible for dev and prod use

--- client/src/services/apollo/client.js
+++ client/src/services/apollo/client.js
@@ -3,8 +3,15 @@ import { createUploadLink } from 'apollo-upload-client';
 import cache from './cache';
 import localStorageService from '@services/localStorage';
 
+// TODO: Fix this mess and make it better automated :D
+
+const uri = process.env.NODE_ENV === 'development'
+  ? 'http://localhost:5000/graphql'
+  : '/graphql';
+
 // const uploadLink = createUploadLink({ uri: 'http://localhost:5000/graphql' });
-const uploadLink = createUploadLink({ uri: 'https://prismicadventures.xyz/graphql' });
+// const uploadLink = createUploadLink({ uri: 'https://prismicadventures.xyz/graphql' });
+const uploadLink = createUploadLink({ uri });
 
 const authMiddleware = new ApolloLink((operation, forward) => {
   operation.setContext(({ headers = {} }) => ({
